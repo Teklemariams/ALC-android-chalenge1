d23217db325f89df053aa8eb4d5abd82098cceff
Teklemariam <teklemarry@gmail.com>
2019-07-11 20:39:21 +0300

    Created the two buttons and textView together with styles, Created AboutALC & Profile Activities !

--- app/src/main/java/com/teklemariam/tekleandalc/WelcomeActivity.java
+++ app/src/main/java/com/teklemariam/tekleandalc/WelcomeActivity.java
@@ -1,13 +1,39 @@
 package com.teklemariam.tekleandalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private Button abAlcButton;
+    private Button myProfileButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        abAlcButton = findViewById(R.id.button_about_alc);
+        myProfileButton= findViewById(R.id.button_my_profile);
+
+        abAlcButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, AboutALCActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeActivity.this, MyProfileActivity.class);
+            }
+        });
+
+
+
     }
 }
