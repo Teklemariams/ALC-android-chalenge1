b7844f2b00f99bc3b8a75ca5602505bdb7c6c7c7
Teklemariam <teklemarry@gmail.com>
2019-07-13 06:52:57 +0300

    ok-Building web apps in WebView

--- app/src/main/java/com/teklemariam/tekleandalc/AboutALCActivity.java
+++ app/src/main/java/com/teklemariam/tekleandalc/AboutALCActivity.java
@@ -1,45 +1,54 @@
 package com.teklemariam.tekleandalc;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class AboutALCActivity extends AppCompatActivity {
-    private WebView alcWebView;
+    private String url = "https://www.google.com/";
+    private WebView webView;
+    //    private WebView alcWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
-/** from codinginflow
 
-        alcWebView = findViewById(R.id.webview_alc);
-        alcWebView.setWebViewClient(new WebViewClient());
-        alcWebView.loadUrl("https://andela.com/alc/");
-//        alcWebView.loadUrl("https://www.google.com/");
+        webView = new WebView(this);
+        setContentView(webView);
+        webView.setWebViewClient(new WebViewClient());
 
-        WebSettings webSettings = alcWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-*/
-//        alcWebView = findViewById(R.id.webview_alc);
-//        alcWebView.loadUrl("https://andela.com/alc/");
-        WebView alcWebView = new WebView(this);
-        setContentView(alcWebView);
-        alcWebView.loadUrl("https://andela.com/alc/");
-        WebSettings webSettings = alcWebView.getSettings();
+        webView.loadUrl(url);
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
 
+
+        class WebAppInterface {
+            Context mContext;
+
+            WebAppInterface(Context c) {
+                mContext = c;
+            }
+
+            @JavascriptInterface
+            public void showToast(String toast) {
+                Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+            }
+        }
+        webView.addJavascriptInterface(new WebAppInterface(this), "Android");
     }
 
-    /**from codinginflow
-     *  @Override
+    @Override
     public void onBackPressed() {
-        if (alcWebView.canGoBack()) {
-            alcWebView.goBack();
+        if (webView.canGoBack()) {
+            webView.goBack();
         } else {
             super.onBackPressed();
         }
-    }*/
+    }
 }
